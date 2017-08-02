7d748c49fa9c4d2170b9abe5743b661afd5647d6
chadfennell <libsys@gmail.com>
2017-08-02 10:58:21 -0500

    initial commit

---
+++ webpack.config.babel.js
@@ -0,0 +1,28 @@
+const { resolve } = require('path');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['react', ['es2015', { modules: false }], 'stage-0'],
+          },
+        },
+      },
+    ],
+  },
+  context: resolve('example'),
+  entry: ['babel-polyfill', './example.js'],
+  devtool: 'source-map',
+  output: {
+    path: resolve('dist'),
+    filename: 'react-citation-demo.js',
+    publicPath: '/dist/',
+    pathinfo: true,
+  },
+};
+
